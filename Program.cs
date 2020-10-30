c1f55a5e64726d91d400fe8099c5a1968195ec4e
Евгений Дулевский <naitmer2003@mail.ru>
2020-10-30 20:35:43 +0300

    Init project

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace FirstRepo
+{
+    class Program
+    {
+        delegate int MyDelegate(int x, int y);
+        delegate void MyDelegate2(int n);
+        static void Main(string[] args)
+        {
+
+            Write("Введите числа\nx=");
+            int x = Convert.ToInt32(ReadLine());
+            Write("y=");
+            int y = Convert.ToInt32(ReadLine());
+            MyDelegate myDelegate = Mult;
+            myDelegate += Sum;
+            WriteLine($"Сумма равна: {myDelegate(x, y)}");
+            myDelegate -= Sum;
+            WriteLine($"Произведение равно: {myDelegate(x, y)}");
+            Write("\nВведите число\nn="); x = Convert.ToInt32(ReadLine());
+            MyDelegate2 myDelegate2 = new MyDelegate2(F1);
+            myDelegate2.Invoke(x);
+        }
+
+        static void F1(int n)
+        {
+            Write(n + ' ');
+            if (n > 3)
+            {
+                F1(n - 1); F1(n / 2);
+            }
+        }
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
